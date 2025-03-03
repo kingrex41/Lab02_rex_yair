184b52ddec9bd2c063ad307e131063acbc963135
yadonis77 <ypalacios22@austincollege.edu>
2025-03-02 22:01:39 -0600

    3d

--- src/Starter.java
+++ src/Starter.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
 
 public class Starter {
 
-	public static void main(String args[]) {
-		System.out.println("Hello World");
+	public static void main(String args[]) {		
+        Scanner scanner = new Scanner(System.in); 
+
+        
+		System.out.println("Hello, please input a number");
+		
+		int firstNum = scanner.nextInt();
+		
+		System.out.println("Now put in another number");
+		
+		int secondNum = scanner.nextInt();
 		
 		
-		System.out.println("HLEOOEOE");
 	}
 	
 }
