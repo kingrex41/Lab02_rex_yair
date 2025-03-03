bec8c21f3685a593836bc24f9422657cccdbb760
Rex King <king.rex41@yahoo.com>
2025-03-02 22:09:22 -0600

    made a sum() method

--- src/Starter.java
+++ src/Starter.java
@@ -2,19 +2,24 @@ import java.util.Scanner;
 
 public class Starter {
 
+	public static int sum (int x, int y) {
+		return x + y;
+	}
+	
 	public static void main(String args[]) {		
         Scanner scanner = new Scanner(System.in); 
 
         
-		System.out.println("Hello, please input a number");
+		System.out.println("Hello, please input a number: ");
 		
 		int firstNum = scanner.nextInt();
 		
-		System.out.println("Now put in another number");
+		System.out.println("Now put in another number: ");
 		
 		int secondNum = scanner.nextInt();
 		
-		int sum = firstNum + secondNum;
+		int sum = sum(firstNum,secondNum);
+		
 		
 		System.out.println("The sum of the integers you entered is: "+sum);
 		
